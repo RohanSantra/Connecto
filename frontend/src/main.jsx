1cb238ef5ec439b44a8950c849c4f79d4295ac00
Rohan <kajalsantra816@gmail.com>
2026-02-09 20:02:46 +0530

    feat: using shadcn sonner

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import { Toaster } from "sonner";
+import { Toaster } from "./components/ui/sonner";
 import App from "./App";
 
 /* Base styles */
@@ -70,7 +70,7 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <BrowserRouter>
             <App />
-            <Toaster position="top-center" richColors />
+            <Toaster position="top-center" />
         </BrowserRouter>
     </ThemeProvider>
 );
